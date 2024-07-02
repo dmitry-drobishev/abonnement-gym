36b2b6a9b6157ae268898ed67c5133a1648df4cb
stooped-dog <mitya.drobishev.77rus@yandex.ru>
2024-07-02 17:28:30 +0300

    добавляет таймер

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,54 @@
+function square(number) {
+  console.log(number * number);
+}
 
-let time = 12000;
+square(10);
 
-// let timerMinutes = document.querySelector(".counter__minutes .counter__numbers")
 
-setInterval( function () {
-  time--
-  if (time = 0) {
-    timerMinutes.textContent = "0"
-  }
-  // timerMinutes.textContent = time;
-  console.log(time);
-}, 1000)
+console.log(101);
+console.log(102);
+console.log(103);
 
+
+// на сколько минут ставим таймер
+var count = 5;
+// запущен таймер или нет
+// started = false;
+
+// запуск таймера по кнопке
+function start() {
+  // если таймер уже запущен — выходим из функции
+  // if (started) {return};
+  // запоминаем время нажатия
+  var start_time = new Date();
+  // получаем время окончания таймера
+  var stop_time = start_time.setMinutes(start_time.getMinutes() + count);
+
+  // запускаем ежесекундный отсчёт
+  var countdown = setInterval(function() {
+    // текущее время
+    var now = new Date().getTime();
+    // сколько времени осталось до конца таймера
+    var remain = stop_time - now;
+    // переводим миллисекунды в минуты и секунды
+    var min = Math.floor( (remain % (1000 * 60 * 60)) / (1000 * 60) );
+    var sec = Math.floor( (remain % (1000 * 60)) / 1000 );
+    // если значение текущей секунды меньше 10, добавляем вначале ведущий ноль
+    sec = sec < 10 ? "0" + sec : sec;
+    // отправляем значение таймера на страницу в нужный раздел
+    document.querySelector(".counter__minutes .counter__numbers").textContent = min;
+    document.querySelector(".counter__seconds .counter__numbers").textContent = sec;
+    // document.getElementById("timer").innerHTML = min + ":" + sec;
+    // если время вышло
+    if (remain < 0) {
+      // останавливаем отсчёт
+      clearInterval(countdown);
+      // пишем текст вместо цифр
+      document.getElementById("timer").innerHTML = "Всё!";
+     }
+  }, 1000);
+  // помечаем, что таймер уже запущен
+  // started = true;
+}
+
+start();
